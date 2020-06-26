daed3d364404e54822cd72c65a01e8073506a314
Do Tuan Anh <dotuananh.dta@gmail.com>
2020-06-26 14:15:34 +0700

    add user info input form to SignUpPage

--- lib/pages/sign_up_page.dart
+++ lib/pages/sign_up_page.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SignUpPage extends StatelessWidget {
+  final formKey = GlobalKey<FormState>();
+
+  final _usernameController = TextEditingController();
+
+  final _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -13,14 +18,91 @@ class SignUpPage extends StatelessWidget {
         ),
         body: BlocBuilder<AppBloc, AppBlocState>(
           builder: (context, state) {
-            return Center(
-              child: Text("Enter Info"),
+            return Stack(
+              fit: StackFit.expand,
+              children: <Widget>[
+                Image.asset(
+                  'assets/bg.jpg',
+                  fit: BoxFit.cover,
+                  color: Colors.black.withOpacity(0.7),
+                  colorBlendMode: BlendMode.darken,
+                ),
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: SingleChildScrollView(
+                      child: SignInForm(
+                          formKey: formKey,
+                          usernameController: _usernameController,
+                          passwordController: _passwordController),
+                    ),
+                  ),
+                ),
+              ],
             );
           },
         ),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.send),
           onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
+
+class SignInForm extends StatelessWidget {
+  const SignInForm({
+    Key key,
+    @required this.formKey,
+    @required TextEditingController usernameController,
+    @required TextEditingController passwordController,
+  })  : _usernameController = usernameController,
+        _passwordController = passwordController,
+        super(key: key);
+
+  final GlobalKey<FormState> formKey;
+  final TextEditingController _usernameController;
+  final TextEditingController _passwordController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                controller: _usernameController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                  hintText: 'Enter email',
+                ),
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                controller: _passwordController,
+                keyboardType: TextInputType.text,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  hintText: 'Enter password',
+                ),
+              ),
+              SizedBox(height: 20),
+              RaisedButton(
+                onPressed: () {
+                  // Constants.prefs.setBool('loggedIn', true);
+                },
+                color: Colors.cyan,
+                child: Text("Sign in"),
+                textColor: Colors.white,
+              )
+            ],
+          ),
         ),
       ),
     );
